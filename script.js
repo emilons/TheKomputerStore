c75875b7966c33f1a00acd489bc5766a0b241a10
emilons <emilons@hotmail.com>
2021-09-07 14:06:37 +0200

    begun with loan

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const bankElement = document.getElementById("bank");
+const loanBlockElement = document.getElementById("loanBlock");
+const loanElement = document.getElementById("loanValue");
 const balanceElement = document.getElementById("balanceValue");
 const payElement = document.getElementById("payValue");
 const loanButton = document.getElementById("loanButton");
 const workButton = document.getElementById("workButton");
 const depositButton = document.getElementById("depositButton");
 
+
 let startingBalance = 0;
 let startingPay = 0;
 let hourlyRate = 100;
+let loanTaken = 0;
+loanBlockElement.style.visibility = "hidden";
 
 balanceElement.innerHTML = startingBalance;
 payElement.innerHTML = startingPay;
+loanElement.innerHTML = loanTaken;
 
+if (parseInt(loanElement.innerHTML) > 0) {
+    loanBlockElement.style.visibility = "visible";
+}
+
+const handleLoan = () => {
+    
+}
 
 // Add functionality so that working after a time increases hourlyRate
 
