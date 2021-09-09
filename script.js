1e8bc409aeb6a470d2d12a7d3041ff2673d799cf
emilons <emilons@hotmail.com>
2021-09-09 15:19:44 +0200

    implemented fetch from RESTful api and selector

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
+// ### DOM elements ###
 const bankElement = document.getElementById("bank");
 const loanBlockElement = document.getElementById("loanBlock");
 const loanElement = document.getElementById("loanValue");
 const balanceElement = document.getElementById("balanceValue");
 const payElement = document.getElementById("payValue");
+const computersElement = document.getElementById("computers");
+const descriptionElement = document.getElementById("description");
 const loanButton = document.getElementById("loanButton");
 const workButton = document.getElementById("workButton");
 const depositButton = document.getElementById("depositButton");
 
-
+// ### Bank functionality ###
 let startingBalance = 0;
-let startingPay = 0;
-let hourlyRate = 100;
 let loanTaken = 0;
 loanBlockElement.style.visibility = "hidden";
 
+
+
 balanceElement.innerHTML = startingBalance;
-payElement.innerHTML = startingPay;
+
 loanElement.innerHTML = loanTaken;
+
+
 
 if (parseInt(loanElement.innerHTML) > 0) {
     loanBlockElement.style.visibility = "visible";
 }
 
-
 const handleLoan = () => {
     let maxLoanAmount = parseInt(balanceElement.innerHTML)*2;
     // if already has a loan...
 }
+
+
+// ### Work functionality ###
+let startingPay = 0;
+let hourlyRate = 100;
+
+
+payElement.innerHTML = startingPay;
 
 // Add functionality so that working after a time increases hourlyRate
 
@@ -44,6 +56,34 @@ const handleDeposit = () => {
     balanceElement.innerHTML = balance;
 }
 
+// ### Computer functionality ###
+
+let computers = []
+
+fetch("https://noroff-komputer-store-api.herokuapp.com/computers")
+    .then(response => response.json())
+    .then(data => computers = data)
+    .then(computers => addComputersToSale(computers));
+
+
+const addComputersToSale = (computers) => {
+    computers.forEach(element => addComputerToSale(element));
+}
+
+const addComputerToSale = (computer) => {
+    const computerElement = document.createElement("option");
+    computerElement.value = computer.id;
+    computerElement.appendChild(document.createTextNode(computer.title));
+    computersElement.appendChild(computerElement);
+}
+
+const handleComputerChange = e => {
+    const selectedComputer = computers[e.target.selectedIndex];
+    descriptionElement.innerHTML = selectedComputer.description;
+}
+
+// ### Set event listeners ###
 loanButton.addEventListener("click", handleLoan);
 workButton.addEventListener("click", handleWork);
 depositButton.addEventListener("click", handleDeposit);
+computersElement.addEventListener("change", handleComputerChange);
